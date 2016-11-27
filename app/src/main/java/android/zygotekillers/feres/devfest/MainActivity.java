4ccfd62839704b8bc97b8c45af704b684d8ac71c
fereshenteti <feressfax@hotmail.com>
2016-11-27 02:05:42 +0100

    feres - home page updated

--- app/src/main/java/android/zygotekillers/feres/devfest/MainActivity.java
+++ app/src/main/java/android/zygotekillers/feres/devfest/MainActivity.java
@@ -43,7 +43,10 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_bookmark) {
+            return true;
+        }
+        else if (id == R.id.action_settings){
             return true;
         }
 
